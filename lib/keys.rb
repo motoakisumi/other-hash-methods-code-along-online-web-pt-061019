aaf5febefb341e78462edea1a4070b0fad1b3fc2
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-28 00:11:21 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -9,10 +9,11 @@ require 'pry'
 # }
 
 def get_the_keys(groceries)
-groceries = {
-  dairy: ["milk", "yogurt", "cheese"],
-  vegetable: ["carrots", "broccoli", "cucumbers"],
-  meat: ["chicken", "steak", "salmon"],
-  grains: ["rice", "pasta"]
-}
+  groceries = {
+    dairy: ["milk", "yogurt", "cheese"],
+    vegetable: ["carrots", "broccoli", "cucumbers"],
+    meat: ["chicken", "steak", "salmon"],
+    grains: ["rice", "pasta"]
+  }
 end
+
